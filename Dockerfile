6f4768e59f2c1ebf0d23ade180bb80f67bd287a7
lilesb <bryanliles@gmail.com>
2019-10-05 09:58:59 -0400

    reworking init files

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
-FROM fedora:30
+FROM ubuntu:18.04
 
+RUN apt update && \
+    apt -q -y --no-install-recommends install neovim nano jq curl net-tools ca-certificates git-core && \
+    rm -rf /var/lib/apt/lists/*
 RUN curl -s -o /usr/local/bin/kubectl -L https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kubectl && \
-    chmod 755 /usr/local/bin/kubectl && \
-    dnf install -q -y which neovim nano jq net-tools
+    chmod 755 /usr/local/bin/kubectl
 
 ARG STERN_VERSION=1.11.0
 
 COPY hacks/init-k8slab.sh /tmp/init-k8slab.sh
-RUN chmod +x /tmp/init-k8slab.sh && \
-    /tmp/init-k8slab.sh && \
+RUN /tmp/init-k8slab.sh && \
     rm -rf /tmp/init-k8slab.sh
 
 USER k8slab
-
 COPY lab_scripts /home/k8slab/lab_scripts
-
 WORKDIR /home/k8slab
+
--- hacks/init-k8slab.sh
+++ hacks/init-k8slab.sh
@@ -2,20 +2,46 @@
 #
 # initialize k8slab container image
 
-set -e
+set -ex
 
-groupadd -g 7777 k8slab
-useradd -m -u 7777 -g k8slab k8slab
-chown k8slab:k8slab /home/k8slab
+# set up k8slab user, paths, aliases
+function setupUser {
+  groupadd -g 7777 k8slab
+  useradd -m -u 7777 -g k8slab k8slab
+  chown k8slab:k8slab /home/k8slab
 
-cat << EOF >> /home/k8slab/.bashrc
+  cat << EOF >> /home/k8slab/.bashrc
 export KUBECONFIG=/home/k8slab/.kube/kind-config-k8s-lab
 alias k='kubectl'
-alias vim='nvim'
 EOF
 
-echo "k8slab ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/k8slab
+}
 
-curl -s -L -o /tmp/stern "https://github.com/wercker/stern/releases/download/${STERN_VERSION}/stern_linux_amd64"
-mv /tmp/stern /usr/local/bin
-chmod 755 /usr/local/bin/stern
+# install stern
+function installStern {
+  curl -s -L -o /tmp/stern "https://github.com/wercker/stern/releases/download/${STERN_VERSION}/stern_linux_amd64"
+  mv /tmp/stern /usr/local/bin
+  chmod 755 /usr/local/bin/stern
+}
+
+# install krew
+installKrew() {
+  (
+    set -x; cd "$(mktemp -d --tmpdir=/work)" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.1/krew.{tar.gz,yaml}" &&
+    tar zxvf krew.tar.gz &&
+    su k8slab -c "./krew-\"$(uname | tr '[:upper:]' '[:lower:]')_amd64\" install --manifest=krew.yaml --archive=krew.tar.gz"
+  )
+  cat << EOF >> /home/k8slab/.bashrc
+export PATH="${KREW_ROOT:-/home/k8slab/.krew}/bin:$PATH"
+EOF
+}
+export -f installKrew
+
+
+setupUser
+installStern
+installKrew
+
+
+exit 0
